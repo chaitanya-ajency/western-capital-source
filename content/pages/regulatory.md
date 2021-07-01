c0ddbdc2ba8c33d0137d5002bef08f1aeb3a718f
chaitanya <chaitanya@ajency.in>
2021-07-01 14:46:43 +0530

    Add site files

---
+++ content/pages/regulatory.md
@@ -0,0 +1,12 @@
+---
+title: "Regulatory | Western Capital Advisors Pvt Ltd"
+date: 2019-08-12T13:13:26+05:30
+type: "pages"
+layout: "regulatory"
+description: "Western Capital, non-banking finance company NBFC in India registered with RBI as a non-deposit taking company serving the NBFC/Fintech to help in their growth capital"
+keyword: "western capital"
+background: "scrolled-down"
+scrolled: "scrolled-down"
+class: "active"
+
+---
---
+++ themes/western-capital/static/js/app.js
@@ -0,0 +1,154 @@
+
+  
+        $(document).ready(function() {
+            $(window).scroll(function() {
+                var scroll = $(window).scrollTop();
+                if (scroll > 50) {
+                    $(".header-1").addClass('scrolled-down').addClass('header-shadow').removeClass('scrolled-top');
+
+                };
+                if (scroll < 50) {
+                    $(".header-1").addClass('scrolled-top').removeClass('scrolled-down').removeClass('header-shadow');
+
+                };
+                if (scroll > 50) {
+                    $(".header-2").addClass('header-shadow');
+
+                };
+                if (scroll < 50) {
+                    $(".header-2").removeClass('header-shadow');
+
+                }
+                
+            })
+        });
+   
+
+  
+        $('.multiple-items').slick({
+            infinite: true,
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            dots: true,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            }, {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }]
+        });
+   
+  
+        $('.testimonial-slide').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            dots: true,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            }, {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+        });
+   
+  
+        $('.meet-team').slick({
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            dots: true,
+            arrows: false,
+            autoplay: false,
+            autoplaySpeed: 2000,
+            responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            }, {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+        });
+   
+  
+        AOS.init();
+   
+  
+        jQuery(document).ready(function($) {
+            $('.counter').counterUp({
+                delay: 5,
+                time: 1000
+            });
+        });
+   
+   
+  
+        $(document).ready(function() {
+
+
+            $(".dropdown-toggle").mouseenter(function() {
+                $('.dropdown-menu').addClass("show-dropdown");
+            });
+            $(".dropdown-menu").mouseleave(function(e) {
+                var value = $(".dropdown-menu").length;
+                if ($(e.target).closest(".dropdown-menu").length === 1) {
+                    $('.dropdown-menu').removeClass("show-dropdown");
+
+                }
+            });
+        });
+   
+  
+        $(document).ready(function() {
+            $(".navbar-toggler").click(function() {
+                $('.navbar').toggleClass("bg-white");
+                $('.header-1 .scrolltop').toggleClass("d-none");
+                $('.header-1 .scrolldown').toggleClass("d-block");
+                $('.fa-bars').toggleClass("text-dark");
+                $('.navbar-toggler').toggleClass("filter-invert");
+            });
+        });
+        $(document).ready(function() {
+            $('.address-2').hide();
+            $(".address-link-2").click(function() {
+                $('.address-2').show();
+            });
+            $(".address-link-1").click(function() {
+                $('.address-2').hide();
+            });
+        });
+   
+        
